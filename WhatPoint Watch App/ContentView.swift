a5b6f86f7084d5fa27c691329d1d8de17516d383
vendor505| <45527907+dudkinox@users.noreply.github.com>
2026-01-03 23:23:11 +0700

    Initial Commit

---
+++ WhatPoint Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WhatPoint Watch App
+//
+//  Created by SORENER on 3/1/2569 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WhatPoint Watch App/WhatPointApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatPointApp.swift
+//  WhatPoint Watch App
+//
+//  Created by SORENER on 3/1/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct WhatPoint_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
